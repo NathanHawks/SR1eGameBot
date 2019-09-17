f3664d4b8c5dbfd2db322a9cd771bdb2553f9133
Nathan Hawks <nhawks@gmail.com>
2019-09-16 17:44:52 -0700

    Opposed rolls!

--- main.js
+++ main.js
@@ -46,6 +46,41 @@ bot.on('messageReactionAdd', (reaction, user) => {
   }
 });
 
+function handleHelpCommand(msg) {
+  msg.reply('GameBot usage:\n'
+    + '!***X***         Roll ***X***d6 *without* exploding 6\'s'
+    + '  ***example:*** !5   rolls 5d6 without exploding\n'
+    + '!X***!***        Roll ***X***d6 ***with*** exploding 6\'s'
+    + '  ***example:*** !5!  rolls 5d6 with exploding\n'
+    + '!X ***tnY***     Roll *without* exploding 6\'s against Target Number ***Y***'
+    + '  ***example:*** !5 tn4   rolls 5d6 w/o exploding vs TN4\n'
+    + '!X! ***tnY***     Roll ***with*** exploding 6\'s against Target Number ***Y***'
+    + '  ***example:*** !5! tn4   rolls 5d6 w/ exploding vs TN4\n'
+    + '\n'
+    + '***NO SPACE between "TN" and number;*** example:\n'
+    + '  !5! TN4     works\n'
+    + '  !5! TN 4    won\'t work\n'
+    + '\n'
+    + 'Notes are OK, and your TN can be in the middle of the note\n'
+    + 'examples:\n'
+    + '  !3! TN4 resist wagemage sorcery      works\n'
+    + '  !3! resist wagemage sorcery TN4      works\n'
+    + '  !3! resist TN4 wagemage sorcery      works\n'
+    + '  resist wagemage sorcery !3! TN4      won\'t work\n'
+    + '\n'
+    + 'Anyone can click the :game_die: reaction to reroll your roll.\n'
+    + 'Remove and re-add your "reaction" to keep re-rolling that roll.\n'
+    + '\n'
+    + 'Opposed Rolls:\n'
+    + '!A! tnB ***vsX!*** ***otnY***\n'
+    + '   Roll *A*d6 (exploding) with tn *B*, opposed by *X*d6 (exploding) with opponent\'s tn *Y*\n'
+    + '   vs*X* = the number of dice the opponent throws (vs*X*! for exploding dice)\n'
+    + '   otn*Y* = the opponent\'s target number\n'
+    + '  ***example:*** !5! tn3 vs6! otn4    Roll 5d6 (exploding) with TN 3, against 6d6 (exploding) with TN 4\n'
+
+  );
+}
+
 function handleMessage(msg, user=msg.author) {
   // check if message starts with `!`
   var message = msg.content;
@@ -56,37 +91,15 @@ function handleMessage(msg, user=msg.author) {
       cmd = cmd.toLowerCase();
       switch(cmd) {
           case 'help':
-            msg.reply('GameBot usage:\n'
-              + '!***X***         Roll ***X***d6 *without* exploding 6\'s'
-              + '  ***example:*** !5   rolls 5d6 without exploding\n'
-              + '!X***!***        Roll ***X***d6 ***with*** exploding 6\'s'
-              + '  ***example:*** !5!  rolls 5d6 with exploding\n'
-              + '!X ***tnY***     Roll *without* exploding 6\'s against Target Number ***Y***'
-              + '  ***example:*** !5 tn4   rolls 5d6 w/o exploding vs TN4\n'
-              + '!X! ***tnY***     Roll ***with*** exploding 6\'s against Target Number ***Y***'
-              + '  ***example:*** !5! tn4   rolls 5d6 w/ exploding vs TN4\n'
-              + '\n'
-              + '***NO SPACE between "TN" and number;*** example:\n'
-              + '  !5! TN4     works\n'
-              + '  !5! TN 4    won\'t work\n'
-              + '\n'
-              + 'Notes are OK, and your TN can be in the middle of the note\n'
-              + 'examples:\n'
-              + '  !3! TN4 resist wagemage sorcery      works\n'
-              + '  !3! resist wagemage sorcery TN4      works\n'
-              + '  !3! resist TN4 wagemage sorcery      works\n'
-              + '  resist wagemage sorcery !3! TN4      won\'t work\n'
-              + '\n'
-              + 'Anyone can click the :game_die: reaction to reroll your roll.'
-            );
+            handleHelpCommand(msg);
           break;
           default:
+            // it's a dice roll
+            // provide reroll ui (dice reaction)
             msg.react('🎲');
-            // it's a dice roll; get setup ===================================
-            //logger.info('cmd = ' + cmd + '; message = ' + message);
+            // get setup ===================================
             // SETUP: how many dice, and do we explode?
             var lastchar = cmd.substring(cmd.length-1, cmd.length);
-            //logger.info('lastchar = ' + lastchar);
             var explode = false;
             var howmany = 0;
             if (lastchar == '!') {
@@ -102,21 +115,51 @@ function handleMessage(msg, user=msg.author) {
             for (x = 0; x < args.length; x++) {
               var firsttwo = args[x].substring(0,2);
               firsttwo = firsttwo.toLowerCase();
-              //logger.info('firsttwo = ' + firsttwo);
               if (firsttwo == 'tn') {
-                //logger.info('TN found: ' + args[x]);
                 tn = args[x].substring(2, args[x].length);
-                //logger.info('TN is: ' + tn);
               }
             }
+            // SETUP: is this an opposed roll?
+            var opposed = false;
+            var opponentdice = -1;
+            var opponenttn = -1;
+            var opposedexplode = false;
+            for (x = 0; x < args.length; x++) {
+              var firsttwo = args[x].substring(0,2);
+              var firstthree = args[x].substring(0,3);
+              firsttwo = firsttwo.toLowerCase();
+              firstthree = firstthree.toLowerCase();
+              if (firsttwo == 'vs') {
+                opposed = true;
+                var lastchar = args[x].substring(args[x].length-1, args[x].length);
+                if (lastchar == '!') {
+                  opposedexplode = true;
+                  opponentdice = args[x].substring(2, args[x].length-1);
+                }
+                else {
+                  opponentdice = args[x].substring(2, args[x].length);
+                }
+
+              }
+              else if (firstthree == 'otn') {
+                opponenttn = args[x].substring(3, args[x].length);
+              }
+            }
+            logger.info('OD: ' + opponentdice + '; OTN: ' + opponenttn + '; OX ' + opposedexplode);
+
             // SETUP: anything remaining is a note; prepare to pass it thru
             var notegiven = false;
             var note = "";
             var spacer = "";
             for (x = 0; x < args.length; x++) {
               var firsttwo = args[x]. substring(0,2);
+              var firstthree = args[x].substring(0,3);
               firsttwo = firsttwo.toLowerCase();
-              if (firsttwo !== 'tn' || notegiven == true) {
+              firstthree = firstthree.toLowerCase();
+              if (
+                (firsttwo !== 'tn' && firsttwo !== 'vs' && firstthree !== 'otn')
+                || notegiven == true
+              ) {
                 notegiven = true;
                 spacer = (note !== "") ? " " : "";
                 note += spacer + args[x];
@@ -125,7 +168,6 @@ function handleMessage(msg, user=msg.author) {
             if (note !== "") note = "(" + note + ")";
             else if (tn > 0) note = "(TN" + tn + ")";
             // GO: Roll dem bones ============================================
-            //logger.info('Dice to roll: ' + howmany + '; explode = ' + explode);
             var successes = 0;
             var rolls = [];
             for (x = 0; x < howmany; x++) {
@@ -134,11 +176,28 @@ function handleMessage(msg, user=msg.author) {
             }
             // Convenience, or hiding terrible RNG? you decide! (it's both)
             rolls.sort(sortNumberDesc);
+            // handle opposed roll
+            if (opposed) {
+              var osuccesses = 0;
+              var orolls = [];
+              for (x = 0; x < opponentdice; x++) {
+                orolls[x] = d6(opposedexplode);
+                if (opponenttn > -1 && orolls[x] >= opponenttn) osuccesses++;
+              }
+              orolls.sort(sortNumberDesc);
+            }
             // prep output and ... put it out
-            var successoutput = "";
-            if (successes > 0) { successoutput = successes + ' successes '; }
-            var output = user + ', you rolled ' +successoutput+ '(' +rolls+ ') ' + note;
-            //logger.info(output);
+            var output = '';
+            if (opposed) {
+              var successoutput = '';
+              if (successes > osuccesses) { successoutput = (successes-osuccesses) + ' net successes '; }
+              output = user + ' rolled ' +successoutput+ '('+rolls+') vs ('+orolls+') ' + note;
+            }
+            else {
+              var successoutput = "";
+              if (successes > 0) { successoutput = successes + ' successes '; }
+              output = user + ', you rolled ' +successoutput+ '(' +rolls+ ') ' + note;
+            }
             msg.channel.send(output);
           break;
        }
