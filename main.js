e6dc8527f9886d80ecc5d39fd6db78eb58db5687
Nathan Hawks <nhawks@gmail.com>
2019-09-24 18:01:48 -0700

    Adding lookahead to opponent TN to handle space

--- main.js
+++ main.js
@@ -145,7 +145,12 @@ function handleMessage(msg, user=msg.author) {
                 else {
                   opponentdice = args[x].substring(2, args[x].length);
                 }
-
+                if (isNaN(Number(otn)) || otn < 2) {
+                  var y = x + 1;
+                  var tmptn = args[y];
+                  if (!isNaN(Number(tmptn)) && tmptn > 1) otn = tmptn;
+                  else otn = -1;
+                 }
               }
               else if (firstthree == 'otn') {
                 opponenttn = args[x].substring(3, args[x].length);
