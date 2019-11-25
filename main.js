f4f44a60684ff16217d4f34b3e20c59e30f91f4b
Nathan Hawks <nhawks@gmail.com>
2019-11-25 15:05:50 -0800

    asdf

--- main.js
+++ main.js
@@ -592,7 +592,7 @@ async function findFileByName(filename, parentID, channelID) {
     {q: `"${parentID}" in parents and name="${filename}"`,
     fields: 'nextPageToken, files(id, name, parents)'},
     (err, res) => {
-      if (err) console.error(err);
+      if (err) return console.error(err);
       if (res.data.files.length === 1) {
         res.data.files.map((file) => {
           global.lastFoundFileID[channelID] = file.id;
